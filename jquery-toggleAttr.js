e22ef0f83d72b582103607051270a134674d266d
George Adamson <george@adamson.com>
2014-03-12 16:10:16 +0000

    Added minified version

--- jquery-toggleAttr.js
+++ jquery-toggleAttr.js
@@ -1,20 +1,11 @@
 // jQuery toggleAttr() method.
 // George Adamson 2014
 // https://github.com/georgeadamson/jQuery-toggleAttr
-// Eg:
-// $('DIV').toggleAttr( 'disabled' )            // Add or remove disabled attribute on each DIV.
-// $(':text').toggleAttr( 'readonly', false )   // Remove readonly attribute on each textbox.
-// $(':checkbox').toggleAttr( 'checked', true)  // Add checked attribute on each checkbox.
-// $(':checkbox').toggleAttr( 'checked', fn )   // Run function on each checkbox to decide whether to add or remove checked attribute.
-//
-// Note: By writing each condition on one line we enable the minimiser to convert them to inline-conditions to reduce code side.
-// This could be written with a fraction less code if the conditions were moved into the loops,
-// but using a separate loop after each condition should be slightly more performant on larger collections.
-
-/* jshint laxcomma:true, asi:true, debug:true, curly:false, camelcase:true, browser:true */
-/* global define */
 
 ;(function (factory) {
+
+  /* jshint laxcomma:true, asi:true, debug:true, curly:false, camelcase:true, browser:true */
+  /* global define */
 
   // Register as an anonymous AMD module if relevant, otherwise assume oldskool browser globals:
   if (typeof define === "function" && define.amd)
@@ -23,6 +14,19 @@
     factory(jQuery);
 
 })(function( $ ) {
+
+  // Eg:
+  // $('DIV').toggleAttr( 'disabled' )            // Add or remove disabled attribute on each DIV.
+  // $(':text').toggleAttr( 'readonly', false )   // Remove readonly attribute on each textbox.
+  // $(':checkbox').toggleAttr( 'checked', true)  // Add checked attribute on each checkbox.
+  // $(':checkbox').toggleAttr( 'checked', fn )   // Run function on each checkbox to decide whether to add or remove checked attribute.
+  //
+  // Note: By writing each condition on one line we enable the minimiser to convert them to inline-conditions to reduce code side.
+  // This could be written with a fraction less code if the conditions were moved into the loops,
+  // but using a separate loop after each condition should be slightly more performant on larger collections.
+
+  /* jshint laxcomma:true, asi:true, debug:true, curly:false, camelcase:true, browser:true */
+  /* global define */
 
   if( !$.fn.toggleAttr ){
 
